e639abbc41c0519f5f980f0e9724463c29dc9c50
hugobigboss2024 <gavanboss2021@gmail.com>
2024-10-03 22:13:45 +0800

    1002

---
+++ demo-sb-restapi/src/main/java/com/bootcamp/demo/demo_sb_restapi/controller/GovCatController.java
@@ -0,0 +1,24 @@
+package com.bootcamp.demo.demo_sb_restapi.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.bootcamp.demo.demo_sb_restapi.dto.GovCatDTO;
+
+@Controller
+@ResponseBody
+public class GovCatController {
+  // Consideration: URL design (based on api consumer and resources)
+  // return List<GovCatDTO>
+  @GetMapping(value = "/govcats")
+  public List<GovCatDTO> getGovCats() {
+    return IntStream.range(1, 5)
+        .mapToObj(i -> new GovCatDTO(10.0 + i))
+        .collect(Collectors.toList());
+  }
+}
---
+++ demo-sb-restapi/src/main/java/com/bootcamp/demo/demo_sb_restapi/dto/GovCatDTO.java
@@ -0,0 +1,12 @@
+package com.bootcamp.demo.demo_sb_restapi.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class GovCatDTO {
+    private double tailLength;
+}
